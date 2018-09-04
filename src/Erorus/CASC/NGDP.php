8271172795744c5131b6e9020f2f3397df7dd828
Erorus <github@everynothing.net>
2018-09-04 13:09:23 -0400

    Add support for Ribbit protocol

--- src/Erorus/CASC/NGDP.php
+++ src/Erorus/CASC/NGDP.php
@@ -2,111 +2,16 @@
 
 namespace Erorus\CASC;
 
-class NGDP {
+class NGDP extends AbstractVersionConfig {
 
     const NGDP_HOST = 'http://us.patch.battle.net:1119/';
-    const MAX_CACHE_AGE = 3600; // 1 hour
 
-    private $region;
-    private $program;
-
-    private $hosts = [];
-    private $cdnPath = '';
-    private $configPath = '';
-
-    private $buildConfig = '';
-    private $cdnConfig = '';
-    private $build = '';
-    private $version = '';
-
-    private $cache;
-
-    public function __construct(Cache $cache, $program='wow', $region='us') {
-        $this->cache = $cache;
-
-        $this->program = strtolower($program);
-        $this->region = strtolower($region);
-    }
-
-    public function getProgram() {
-        return $this->program;
-    }
-
-    public function getRegion() {
-        return strtoupper($this->region);
-    }
-
-    public function getHosts() {
-        if (!$this->hosts) {
-            $this->getCDNs();
-        }
-
-        return $this->hosts;
-    }
-
-    public function getCDNPath() {
-        if (!$this->cdnPath) {
-            $this->getCDNs();
-        }
-
-        return $this->cdnPath;
-    }
-    public function getConfigPath() {
-        if (!$this->configPath) {
-            $this->getCDNs();
-        }
-
-        return $this->configPath;
-    }
-
-    public function getBuildConfig() {
-        if (!$this->buildConfig) {
-            $this->getVersions();
-        }
-
-        return $this->buildConfig;
-    }
-
-    public function getCDNConfig() {
-        if (!$this->cdnConfig) {
-            $this->getVersions();
-        }
-
-        return $this->cdnConfig;
-    }
-
-    public function getBuild() {
-        if (!$this->build) {
-            $this->getVersions();
-        }
-
-        return $this->build;
-    }
-
-    public function getVersion() {
-        if (!$this->version) {
-            $this->getVersions();
-        }
-
-        return $this->version;
-    }
-
-    private function getCachedResponse($cachePath, $maxAge = false) {
-        if (!$this->cache->fileExists($cachePath)) {
-            return false;
-        }
-        if ($maxAge && $this->cache->fileModified($cachePath) < (time() - $maxAge)) {
-            return false;
-        }
-        return $this->cache->readPath($cachePath);
-    }
-
-    private function getNGDPData($file) {
-        $cachePath = 'ngdp/' . $this->program . '/' . $file;
+    protected function getNGDPData($file) {
+        $cachePath = 'ngdp/' . $this->getProgram() . '/' . $file;
 
         $data = $this->getCachedResponse($cachePath, static::MAX_CACHE_AGE);
         if (!$data) {
-            $url  = sprintf('%s%s/%s', static::NGDP_HOST, $this->program, $file);
+            $url  = sprintf('%s%s/%s', static::NGDP_HOST, $this->getProgram(), $file);
             $data = HTTP::Get($url);
             if (!$data) {
                 $data = $this->getCachedResponse($cachePath);
@@ -117,100 +22,4 @@ class NGDP {
 
         return $data;
     }
-
-    private function getCDNs()
-    {
-        $data = $this->getNGDPData('cdns');
-        if ( ! $data) {
-            return;
-        }
-
-        $lines = preg_split('/[\r\n]+/', $data);
-        if (strpos($lines[0], '|') === false) {
-            return;
-        }
-        $cols = explode('|', strtolower($lines[0]));
-        $names = [];
-        foreach ($cols as $col) {
-            $name = $col;
-            if (($pos = strpos($name, '!')) !== false) {
-                $name = substr($name, 0, $pos);
-            }
-            $names[] = $name;
-        }
-
-        for ($x = 1; $x < count($lines); $x++) {
-            $vals = explode('|', $lines[$x]);
-            if (count($vals) != count($names)) {
-                continue;
-            }
-            $row = array_combine($names, $vals);
-            if (!isset($row['name'])) {
-                continue;
-            }
-            if ($row['name'] != $this->region) {
-                continue;
-            }
-            if (isset($row['path'])) {
-                $this->cdnPath = $row['path'];
-            }
-            if (isset($row['hosts'])) {
-                $this->hosts = explode(' ', $row['hosts']);
-            }
-            if (isset($row['configpath'])) {
-                $this->configPath = $row['configpath'];
-            }
-            break;
-        }
-    }
-
-    private function getVersions()
-    {
-        $data = $this->getNGDPData('versions');
-        if ( ! $data) {
-            return;
-        }
-
-        $lines = preg_split('/[\r\n]+/', $data);
-        if (strpos($lines[0], '|') === false) {
-            return;
-        }
-        $cols = explode('|', strtolower($lines[0]));
-        $names = [];
-        foreach ($cols as $col) {
-            $name = $col;
-            if (($pos = strpos($name, '!')) !== false) {
-                $name = substr($name, 0, $pos);
-            }
-            $names[] = $name;
-        }
-
-        for ($x = 1; $x < count($lines); $x++) {
-            $vals = explode('|', $lines[$x]);
-            if (count($vals) != count($names)) {
-                continue;
-            }
-            $row = array_combine($names, $vals);
-            if (!isset($row['region'])) {
-                continue;
-            }
-            if ($row['region'] != $this->region) {
-                continue;
-            }
-            if (isset($row['buildconfig'])) {
-                $this->buildConfig = $row['buildconfig'];
-            }
-            if (isset($row['cdnconfig'])) {
-                $this->cdnConfig = $row['cdnconfig'];
-            }
-            if (isset($row['buildid'])) {
-                $this->build = $row['buildid'];
-            }
-            if (isset($row['versionsname'])) {
-                $this->version = $row['versionsname'];
-            }
-            break;
-        }
-    }
-
 }
---
+++ src/Erorus/CASC/Ribbit.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Erorus\CASC;
+
+use \ZBateson\MailMimeParser\MailMimeParser;
+use \ZBateson\MailMimeParser\Message\Part\MessagePart;
+
+class Ribbit extends AbstractVersionConfig {
+
+    const RIBBIT_HOST = 'us.version.battle.net';
+    const RIBBIT_PORT = 1119;
+    const TIMEOUT = 10; // seconds
+
+    protected function getNGDPData($file) {
+        $cachePath = 'ribbit/' . $this->getProgram() . '/' . $file;
+
+        $command = sprintf("v1/products/%s/%s\n", $this->getProgram(), $file);
+
+        $data = $this->getCachedResponse($cachePath, static::MAX_CACHE_AGE);
+        if ($data) {
+            return $data;
+        }
+
+        $errNum = 0;
+        $errMsg = '';
+        $handle = fsockopen(static::RIBBIT_HOST, static::RIBBIT_PORT, $errNum, $errMsg, static::TIMEOUT);
+        if ($handle !== false) {
+            stream_set_timeout($handle, static::TIMEOUT);
+
+            fwrite($handle, $command);
+
+            $parser = new MailMimeParser();
+            $message = $parser->parse($handle);
+
+            fclose($handle);
+
+            /** @var MessagePart[] $attachments */
+            $attachments = $message->getAllAttachmentParts();
+            foreach ($attachments as $attachment) {
+                // If we look for "cdns", the content-disposition will be "cdn"
+                if (strpos($file, $attachment->getContentDisposition()) !== 0) {
+                    continue;
+                }
+
+                $data = $attachment->getContent();
+                break;
+            }
+        }
+
+        if ($data) {
+            $this->cache->writePath($cachePath, $data);
+        } else {
+            $data = $this->getCachedResponse($cachePath);
+        }
+
+        return $data;
+    }
+}
