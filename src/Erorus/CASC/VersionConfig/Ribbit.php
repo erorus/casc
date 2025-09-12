e5268218c95cc05e177598ad38ee0b1517d2e3f8
Erorus <erorus@users.noreply.github.com>
2025-09-12 00:54:21 -0400

    use ribbit v2

--- src/Erorus/CASC/VersionConfig/Ribbit.php
+++ src/Erorus/CASC/VersionConfig/Ribbit.php
@@ -2,15 +2,12 @@
 
 namespace Erorus\CASC\VersionConfig;
 
+use Erorus\CASC\HTTP as CASCHTTP;
 use Erorus\CASC\VersionConfig;
-use ZBateson\MailMimeParser\MailMimeParser;
-use ZBateson\MailMimeParser\Message\Part\MessagePart;
 
 class Ribbit extends VersionConfig {
 
-    private const RIBBIT_HOST = 'us.version.battle.net';
-    private const RIBBIT_PORT = 1119;
-    private const TIMEOUT = 10; // seconds
+    private const URL_PREFIX = 'https://us.version.battle.net/v2';
 
     /**
      * Returns the content of a version config file at the given path, either from cache or by fetching it directly.
@@ -20,36 +17,16 @@ class Ribbit extends VersionConfig {
      * @return string|null
      */
     protected function getTACTData(string $file): ?string {
-        $cachePath = 'ribbit/' . $this->getProgram() . '/' . $file;
-
-        $command = sprintf("v1/products/%s/%s\n", $this->getProgram(), $file);
+        $cachePath = 'ribbit-v2/' . $this->getProgram() . '/' . $file;
 
         $data = $this->getCachedResponse($cachePath, static::MAX_CACHE_AGE);
-        if ($data) {
-            return $data;
-        }
-
-        $errNum = 0;
-        $errMsg = '';
-        $handle = fsockopen(static::RIBBIT_HOST, static::RIBBIT_PORT, $errNum, $errMsg, static::TIMEOUT);
-        if ($handle !== false) {
-            stream_set_timeout($handle, static::TIMEOUT);
-
-            fwrite($handle, $command);
-
-            $parser = new MailMimeParser();
-            $message = $parser->parse($handle, true);
-
-            /** @var MessagePart[] $attachments */
-            $attachments = $message->getAllAttachmentParts();
-            foreach ($attachments as $attachment) {
-                // If we look for "cdns", the content-disposition will be "cdn"
-                if (strpos($file, $attachment->getContentDisposition()) !== 0) {
-                    continue;
-                }
-
-                $data = $attachment->getContent();
-                break;
+        if (!$data) {
+            $url = self::URL_PREFIX . sprintf("/products/%s/%s", $this->getProgram(), $file);
+            try {
+                $data = CASCHTTP::get($url);
+            } catch (\Exception $e) {
+                echo "\n - " . $e->getMessage() . " ";
+                $data = '';
             }
         }
 
